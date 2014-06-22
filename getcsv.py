c30ae5bd395adbbafe8f66e4e2ebd1132ebd116e
Ege Demirel <egedemirel@gmail.com>
2014-06-22 07:43:56 -0500

    Update getcsv.py

--- getcsv.py
+++ getcsv.py
@@ -34,41 +34,23 @@ def upload_csv_to_redis(csv_filename):
 if __name__ == "__main__": 
 
 	#CONFIG
-	#r = redis.StrictRedis(host='138.91.93.45', port='6379', db=0)
 	r = redis.StrictRedis(host='localhost', port='6379', db=0)
 
+	#I reset the Redis db every run, for no appearent reason
 	r.flushdb()
+	
 
-	#CLOUDURL = 'http://192.168.0.230:54388/'
-	# CLOUDURL = "http://egeneo4j.cloudapp.net/test"
-	CSVSTORE = "http://egeneo4j.cloudapp.net/static/"
-
-	# #Read HTML
-	# f = urllib2.urlopen(CLOUDURL)	
-	# html_file = f.read()
-
-	# # #Convert HTML to BeautifulSoup
-	# soup = BeautifulSoup(html_file)
-
-	list_csv = os.listdir("/home/Ege/hackathon/samplewebserver/static/")
+	CSVSTORE = "http://localhost/static/"
 
 
-	# # #Search for all "a" tags, get href attributes
-	# for link in soup.findAll("a"):
-	#  	#get attribute in href tag, combine with base link
-	#  	#the reason for [8:-1] is dumping the /static/ text
-	# 	if(link["href"].find("+Delete") == -1):
-	# 		csvlink = CLOUDURL+link["href"]
-	# 		list_csv.append(str(link["href"][8:]))
+	list_csv = os.listdir("<DIRECTORY_WHERE_YOU_PUT_YOUR_ACCELEROMETER DATA")
 
-	#print list_csv
+
 	try:
 		for i in range(0,len(list_csv)-1):
 			upload_csv_to_redis(list_csv[i])
 			print 'uploaded ' + list_csv[i]
 	except Exception, e:
-		print list_csv[i] + " patladi, canin sagolsun"
+		print list_csv[i] + " nope, that didn't happen"
 	
 
-
-	
