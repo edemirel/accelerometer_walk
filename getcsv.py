b13597cc2e04e46fbc6b9a0f6cc0e612acf4d4e5
Michael Pedersen <mp@beefy.dk>
2014-06-23 21:38:26 +0200

    Use a context manager to ensure closing file
    This will ensure that the resource is always closed, even if there
    is an exception. Normal files support this out of the box, but urlopen
    doesn't (until python 3), so we have to wrap it.
    
    See https://docs.python.org/2/library/contextlib.html

--- getcsv.py
+++ getcsv.py
@@ -2,6 +2,7 @@
 import os
 import csv
 import urllib2
+import contextlib
 
 import redis
 
@@ -9,34 +10,34 @@ import redis
 CSVSTORE = "http://localhost/static/"
 
 
-def upload_csv_to_redis(csv_filename):
+def upload_csv_to_redis(csvfile):
     # explode filename into parts
-    file_name = csv_filename.replace(".", "_").split("_")
+    file_name = csvfile.replace(".", "_").split("_")
 
-    csv_f = urllib2.urlopen(CSVSTORE + csv_filename)
-    reader = csv.reader(csv_f)
+    with contextlib.closing(urllib2.urlopen(csvfile)) as f:
+        reader = csv.reader(f)
 
-    # open the CSV file from HTML stream
-    rownum = 0
+        # open the CSV file from HTML stream
+        rownum = 0
 
-    # create generic redis line for specific file
-    redisline = 'accel:raw:' + str(file_name[1]) + ':' + str(file_name[2]) + ':'
+        # create generic redis line for specific file
+        redisline = 'accel:raw:' + str(file_name[1]) + ':' + str(file_name[2]) + ':'
 
-    for row in reader:
-        if rownum == 0:
-            header = row
+        for row in reader:
+            if rownum == 0:
+                header = row
 
-        else:
-            colnum = 0
-            # prepare redis key for that row
+            else:
+                colnum = 0
+                # prepare redis key for that row
 
-            for word in row:
-                r.hset(redisline + str(rownum), header[colnum], word)
-                r.sadd('keys', redisline + str(rownum))
-                # print redisline + str(rownum)
-                colnum += 1
+                for word in row:
+                    r.hset(redisline + str(rownum), header[colnum], word)
+                    r.sadd('keys', redisline + str(rownum))
+                    # print redisline + str(rownum)
+                    colnum += 1
 
-        rownum += 1
+            rownum += 1
 
 
 if __name__ == "__main__":
