03035edcf687a3e43596afe33d2456b66f247e08
Michael Pedersen <mp@beefy.dk>
2014-06-23 21:48:31 +0200

    Use enumerate instead of counting manually
    Faster, more pythonic, easier to read, less bookkeeping

--- getcsv.py
+++ getcsv.py
@@ -17,27 +17,17 @@ def upload_csv_to_redis(csvfile):
     with contextlib.closing(urllib2.urlopen(csvfile)) as f:
         reader = csv.reader(f)
 
-        # open the CSV file from HTML stream
-        rownum = 0
-
         # create generic redis line for specific file
         redisline = 'accel:raw:' + str(file_name[1]) + ':' + str(file_name[2]) + ':'
 
-        for row in reader:
-            if rownum == 0:
+        for r, row in enumerate(reader):
+            if r == 0:
                 header = row
-
             else:
-                colnum = 0
-                # prepare redis key for that row
-
-                for word in row:
-                    r.hset(redisline + str(rownum), header[colnum], word)
-                    r.sadd('keys', redisline + str(rownum))
+                for c, word in enumerate(row):
+                    r.hset(redisline + str(r), header[c], word)
+                    r.sadd('keys', redisline + str(r))
                     # print redisline + str(rownum)
-                    colnum += 1
-
-            rownum += 1
 
 
 if __name__ == "__main__":
